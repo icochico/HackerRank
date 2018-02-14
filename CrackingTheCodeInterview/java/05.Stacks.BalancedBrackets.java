bf009e347c774c94351bc3ed4327536381675c6d
ecasini <ecasini@ihmc.us>
2018-02-14 16:57:53 -0600

    Added 05. Balanced Brackets

---
+++ CrackingTheCodeInterview/java/05.Stacks.BalancedBrackets.java
@@ -0,0 +1,81 @@
+// A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+//
+// Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+//
+// A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+//
+// Some examples of balanced brackets are []{}(), [({})]{}() and ({(){}[]})[].
+//
+// By this logic, we say a sequence of brackets is considered to be balanced if the following conditions are met:
+//
+// It contains no unmatched brackets.
+// The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+// Given  strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, print YES on a new line; otherwise, print NO on a new line.
+
+// Sample Input
+//
+// 3
+// {[()]}
+// {[(])}
+// {{[[(())]]}}
+// Sample Output
+//
+// YES
+// NO
+// YES
+// Explanation
+//
+// The string {[()]} meets both criteria for being a balanced string, so we print YES on a new line.
+// The string {[(])} is not balanced, because the brackets enclosed by the matched pairs [(] and (]) are not balanced. Thus, we print NO on a new line.
+// The string {{[[(())]]}} meets both criteria for being a balanced string, so we print YES on a new line.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static boolean isBalanced(String expression) {
+
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(new Character(c));
+            }
+            else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+            else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+            else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+
+        //because there could still be character in the stack (uneven case)
+        if (!stack.isEmpty()) return false;
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            String expression = in.next();
+            System.out.println( (isBalanced(expression)) ? "YES" : "NO" );
+        }
+    }
+}
